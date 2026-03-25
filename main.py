d79b2a7354d095755e202ba2555fd882ad3f9144
Takumi <itriandaiskida@gmail.com>
2026-03-26 02:16:49 +0900

    7

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+count = 1
+while count < 4:
+    print(count)
+    count += 1
 
-for i in range(1,6):
-    print(f"{i}:Hello Python")
+j = 0
+while j < 5:
+    print(j)
+    j += 1
+i = 0
+while i < 4:
+    print("Hello")
+    i += 1
