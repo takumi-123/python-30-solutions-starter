55d2d70a715baca2ad39b20b8d83f095730bab28
Takumi <itriandaiskida@gmail.com>
2026-03-26 11:27:53 +0900

    11

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-number = [1, 2, 3]
-print(number[1])
-number[2] = 4
-print(number[2])
+fruits = "apple banana orange"
+word = fruits.split(" ")
+print(word)
+
+mozi = {"Hello", "World", "python"}
+word2 = " ".join(mozi)
+print(word2)
+
+number = "one two three"
+word3 = number.split()
+number_s = "_".join(word3)
+print(number_s)
