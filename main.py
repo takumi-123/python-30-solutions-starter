e7894f1487cec43942799dd7b37a7557cb447d65
Takumi <itriandaiskida@gmail.com>
2026-03-26 11:00:55 +0900

    9

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-def print_hello_python():
-    print("Hello Python")
-
-for i in range(3):
-    print_hello_python()
-
-def say_hello(name):
-    print(f"Hello {name}")
-    return name
-say_hello("Alice")
-
-def format_name(name2):
-    print(f"Hello {name2}")
-    return name2
-format_name("太郎")
+number = [1, 2, 3]
+print(number[1])
+number[2] = 4
+print(number[2])
