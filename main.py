c8d8f227819e0434a997b6fa2dd7514deb8f1cf8
Takumi <itriandaiskida@gmail.com>
2026-03-27 23:43:49 +0900

    14

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-with open("output.txt", mode="w", encoding="utf-8") as f:
-    f.write("Hello, Python!")
+try:
+    user_input = input("整数を入力してください")
+    number = int(user_input)
+    print(f"入力された数値は{number}です")
+except ValueError:
+    print("入力された値を整数に変換できません。整数を入力してください。")
 
-with open("output.txt", mode="r", encoding="utf=8") as f:
-    content = f.read()
-    print(content)
-
-with open("output.txt", mode="a", encoding="utf=8") as f:
-    f.write("\nThis is a new line.")
-with open("output.txt", mode="r", encoding="utf-8") as f:
-    print(f.read())
+#2
+while True:
+    try:
+        users_input = input("整数を入力してください")
+        numbers = int(users_input)
+        print(f"あなたの入力した整数は{numbers}です")
+        break
+    except ValueError:
+        print("入力された値を整数に変換できません。整数を入力してください。") 
+               
