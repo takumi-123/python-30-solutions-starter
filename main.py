8eadd48136bf71c9c0ef79121bd41d23e4aa54a2
Takumi <itriandaiskida@gmail.com>
2026-03-31 11:09:44 +0900

    25

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-import re
+import logging
 
 def main():
-    #テキスト定義
-    text = """
-Contact list:
-John Doe's email is john.doe@example.com.
-Jane Smith can be reached at jane.smith@example.org.
-For inquiries, please contact info@company.co.uk.
-Invalid email: user@.com or @domain.com
-"""
-
-    #メアドのパターン定義
-    pattern = r"[\w\.-]+@[\w\.-]+"
-    emails = re.findall(pattern, text)
-
-    for email in emails:
-        print(email)
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(message)s")
+    #2.ログメッセージの出力
+    logging.info("Processing data...")
+    logging.warning("something unpxected happend.")
 
 if __name__ == "__main__":
     main()
-
